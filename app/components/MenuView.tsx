fd095512cf6298496edd67f56c27a9a6f3e3d4c8
Ezequiel Lino <elino@xquisito.ai>
2026-01-08 16:47:23 -0600

    mejora para visualizacion

--- app/components/MenuView.tsx
+++ app/components/MenuView.tsx
@@ -137,7 +137,7 @@ function MenuView({ tableNumber }: MenuViewProps) {
 
       <MenuHeader restaurant={restaurant} tableNumber={tableNumber} />
 
-      <main className="mt-48 md:mt-64 lg:mt-80 relative z-10" style={{marginTop:'11rem'}}>
+      <main className="mt-48 md:mt-64 lg:mt-80 relative z-10" style={{marginTop:'9rem'}}>
         <div className="bg-white rounded-t-4xl flex flex-col items-center px-6 md:px-8 lg:px-10">
           <div className="mt-6 md:mt-8 flex items-start justify-between w-full">
             {/* Settings Icon */}
